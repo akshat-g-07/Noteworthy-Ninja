1eec8efe289a27032311a336a9ecd5397172e632
Akshat Garg <akshatg805@gmail.com>
2024-08-03 15:55:23 +0530

    feat: context menu setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getAuthToken, getUserInfo } from "./chromeActions";
 
 function App() {
   const [userInfo, setUserInfo] = useState(null);
   const [error, setError] = useState(null);
+  const [noteString, setNoteString] = useState("");
+
+  useEffect(() => {
+    chrome.storage.session.get("noteString", ({ noteString }) => {
+      updateDefinition(noteString);
+    });
+
+    const listener = (changes) => {
+      const noteStringChange = changes["noteString"];
+      if (noteStringChange) {
+        updateDefinition(noteStringChange.newValue);
+      }
+    };
+
+    chrome.storage.session.onChanged.addListener(listener);
+
+    return () => {
+      chrome.storage.session.onChanged.removeListener(listener);
+    };
+  }, []);
+
+  function updateDefinition(newWord) {
+    if (!newWord) return;
+
+    setNoteString(newWord);
+  }
 
   const authenticate = async () => {
     try {
@@ -22,6 +48,7 @@ function App() {
       <div className="h-screen w-screen bg-neutral-500 rounded p-4">
         <button onClick={authenticate}>Authenticate</button>
         {userInfo && <>{JSON.stringify(userInfo)}</>}
+        {noteString && <>{JSON.stringify(noteString)}</>}
         {error && <>{JSON.stringify(error)}</>}
       </div>
     </>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "src/service-worker.js"),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name === "background"
+            ? "[name].js"
+            : "[name]-[hash].js";
+        },
+      },
+    },
+  },
+});
