5d1a9628d39ad60cca4c6b1079fc871ab1484ff8
Akshat Garg <akshatg805@gmail.com>
2024-08-12 11:48:05 +0530

    feat: react router dom setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+
 import "./App.css";
+import "./index.css";
+
+import App from "./App.jsx";
+import Home from "./components/home.jsx";
+import Auth from "./components/auth.jsx";
+import Payment from "./components/payment.jsx";
+import Notepad from "./components/notepad.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/home", element: <Home /> },
+      { path: "/auth", element: <Auth /> },
+      { path: "/payment", element: <Payment /> },
+      { path: "/notepad", element: <Notepad /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
