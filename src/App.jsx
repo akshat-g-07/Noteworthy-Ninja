2f94352d4e6f141f3d944be01398e064fae87170
Akshat Garg <akshatg805@gmail.com>
2024-09-02 22:46:42 +0530

    style: css for y scroll

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function App() {
   return (
     <>
-      <div className="w-screen min-h-screen bg-stone-700 p-10">
+      <div className="w-screen h-screen bg-stone-700 p-10">
         <Outlet />
       </div>
     </>
