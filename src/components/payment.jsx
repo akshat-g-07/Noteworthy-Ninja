32b534eae81e17209bde0488e2baf30a6baef2c4
Akshat Garg <akshatg805@gmail.com>
2024-09-02 22:45:17 +0530

    feat: payment server
    => created a different payment server as chrome extensions doesn't support paypal scripts.

--- src/components/payment.jsx
+++ src/components/payment.jsx
@@ -22,20 +22,18 @@ import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 export default function Payment() {
   const location = useLocation();
-  const { user } = location.state?.key || {};
+  const { user, token } = location.state?.key || {};
   const navigate = useNavigate();
 
-  const initialOptions = {
-    "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID,
-    currency: "USD",
-    intent: "subscription",
-  };
+  // const initialOptions = {
+  //   "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID,
+  //   currency: "USD",
+  //   intent: "subscription",
+  // };
 
   useEffect(() => {
     if (!user) {
-      navigate("/", {
-        state: { key: { error: "not logged in" } },
-      });
+      navigate("/");
       return;
     }
 
@@ -69,10 +67,56 @@ export default function Payment() {
             subscribed: "NotSubscribed",
           }),
         });
+
+        chrome.windows.create(
+          {
+            url: `${import.meta.env.VITE_PAYMENT_SERVICE}/?token=${token}`,
+            type: "popup",
+            width: 500,
+            height: 600,
+          },
+          (window) => {
+            // You can handle the newly created window here if needed
+          }
+        );
       }
     };
 
     checkSubscriptionStatus();
+
+    // Add message listener
+    const messageListener = (message) => {
+      if (message.type === "PAYMENT_SUCCESS") {
+        console.log("Payment successful");
+        // Update user subscription status
+        fetch(import.meta.env.VITE_PYTHON_SERVICE + "/user_subscribed", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userEmail: user.email,
+          }),
+        }).then(() => {
+          navigate("/notepad");
+        });
+      } else if (message.type === "PAYMENT_FAILURE") {
+        console.error("Payment failed:", message.error);
+        chrome.notifications.create({
+          type: "basic",
+          title: "Payment Failed",
+          message: "Payment failed. Please try again.",
+        });
+      }
+    };
+
+    // Add the listener
+    chrome.runtime.onMessage.addListener(messageListener);
+
+    // Remove the listener when the component unmounts
+    return () => {
+      chrome.runtime.onMessage.removeListener(messageListener);
+    };
   }, [user, navigate]);
 
   // const handleLogout = async () => {
@@ -94,6 +138,29 @@ export default function Payment() {
             Noteworthy Ninja
           </p>
         </div>
+        <div className="flex justify-center items-center h-full mt-20">
+          <p className="text-white text-3xl font-bold">
+            $9<span className="text-sm">/month</span>
+          </p>
+        </div>
+        <button
+          className="bg-[#facc15] hover:bg-[#facc15]/90 text-black px-4 py-2 rounded-md flex items-center font-medium shadow-md border border-black mt-10 mx-auto"
+          onClick={() => {
+            chrome.windows.create(
+              {
+                url: `${import.meta.env.VITE_PAYMENT_SERVICE}/?token=${token}`,
+                type: "popup",
+                width: 500,
+                height: 600,
+              },
+              (window) => {
+                // You can handle the newly created window here if needed
+              }
+            );
+          }}
+        >
+          Subscribe
+        </button>
         {/* 
         <PayPalScriptProvider options={initialOptions}>
           <PayPalButtons
